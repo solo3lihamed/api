14b98541a2616c74c31f0b20bd2b1e85bb9d0c9f
solo3lihamed <fps60y@gmail.com>
2025-12-14 18:58:54 +0200

    ProgramViewSet

--- school/views.py
+++ school/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import viewsets,permissions
+
+from .serializers import (
+    ProgramSerializer,EventSerializer,TeatcherSerializer,
+    TestimonialSerializer,StudentSerializer,GradeSerializer,
+    ReviewSerializer
+) 
+
+from .models import Program,Event,Teacher,Testimonial,Student,Grade,Review
+
 
 
 
 # Create your views here.
 def hello_world(request):
     return HttpResponse("hello hamed")
+
+
+
+class ProgramViewSet(viewsets.ModelViewSet):
+    queryset = Program.objects.all()
+    serializer_class = ProgramSerializer
