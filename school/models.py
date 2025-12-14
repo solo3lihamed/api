657fba2c1b90c8fc6a7fa14397b69d82c530f3e3
solo3lihamed <fps60y@gmail.com>
2025-12-14 14:44:52 +0200

    Student model

--- school/models.py
+++ school/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -41,4 +42,13 @@ class Teacher(models.Model):
         return self.name
     
 
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    grade_level = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.user.username
+    
     
